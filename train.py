b6c27e3e00d230a7b2b66a5ef6a9fa64d9a2ecfc
nidhogg <424543573@qq.com>
2024-04-17 13:27:34 +0800

    test_git

---
+++ train.py
@@ -0,0 +1,14 @@
+#coding:utf-8
+from ultralytics import YOLO
+
+# 加载预训练模型
+# 添加注意力机制，SEAtt_yolov8.yaml 默认使用的是n。
+# SEAtt_yolov8s.yaml，则使用的是s，模型。
+model = YOLO("ultralytics/cfg/models/v8/yolov8n-SEAtt.yaml").load('yolov8n.pt')
+
+# Use the model
+if __name__ == '__main__':
+    # Use the model
+    results = model.train(data='ultralytics/cfg/datasets/coco8.yaml', epochs=3)  # 训练模型
+    # 将模型转为onnx格式
+    # success = model.export(format='onnx')
