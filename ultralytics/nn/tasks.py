4eae132f1e1481cb7ec529e3bb10bf0868c8ff65
nidhogg <424543573@qq.com>
2024-04-19 11:47:04 +0800

    test

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -921,7 +921,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
         elif m is RTDETRDecoder:  # special case, channels arg must be passed in index 1
             args.insert(1, [ch[x] for x in f])
         elif m in {SEAttention}:
-            args = [ch[f], *args]
+            c1, c2 = ch[f], args[0]
         elif m in {CoordAtt}:
             args = [ch[f], *args]
 
