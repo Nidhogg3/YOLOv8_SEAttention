7b1365f1e856c3370d7471c3459b5a64adb8d358
nidhogg <424543573@qq.com>
2024-04-21 12:15:15 +0800

    test

--- train-local.py
+++ train-local.py
@@ -4,11 +4,11 @@ from ultralytics import YOLO
 # 加载预训练模型
 # 添加注意力机制，SEAtt_yolov8.yaml 默认使用的是n。
 # SEAtt_yolov8s.yaml，则使用的是s，模型。
-model = YOLO("/kaggle/working/YOLOv8_SEAttention/ultralytics/cfg/models/v8/yolov8-GAM.yaml").load('yolov8n.pt')
+model = YOLO("ultralytics/cfg/models/v8/yolov8n-SEAtt.yaml").load('yolov8n.pt')
 
 # Use the model
 if __name__ == '__main__':
     # Use the model
-    results = model.train(data='/kaggle/working/voc-yolo/VOC.yaml', epochs=300, device=[0, 1], batch=64, cache=True, workers=8, patience=50)  # 训练模型
+    results = model.train(data='coco8.yaml', pretrained='yolov8n.pt', epochs=3)  # 训练模型
     # 将模型转为onnx格式
     # success = model.export(format='onnx')
